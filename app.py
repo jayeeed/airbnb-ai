1c5bb6955586889cc22805c2cfaa8f5031e80582
jayeeed <jayedbinjahangir@gmail.com>
2024-01-04 16:56:09 +0600

    init

--- app.py
+++ app.py
@@ -17,6 +17,5 @@ app.register_blueprint(voice_search_route)
 app.register_blueprint(sentiment_route)
 app.register_blueprint(FAQ_chat_route)
 
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
