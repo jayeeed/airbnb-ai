0d842bcd7356825c4f2e8634ec23e31ead1675f4
jayeeed <jayedbinjahangir@gmail.com>
2024-01-04 18:14:45 +0600

    all

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from routes.sentiment_routes import sentiment_route
 from routes.voice_search_routes import voice_search_route
 from routes.recommend_routes import recommend_route
 from routes.property_routes import property_route
-# from routes.price_prediction_routes import price_predict_routes
-# from routes.instant_varification_routes import verify_routes
+from routes.price_prediction_routes import price_predict_routes
+from routes.instant_varification_routes import verify_routes
 from routes.FAQ_chat import FAQ_chat_route
 
 app = Flask(__name__)
 
 app.register_blueprint(recommend_route)
 app.register_blueprint(property_route)
-# app.register_blueprint(price_predict_routes)
-# app.register_blueprint(verify_routes)
+app.register_blueprint(price_predict_routes)
+app.register_blueprint(verify_routes)
 app.register_blueprint(voice_search_route)
 app.register_blueprint(sentiment_route)
 app.register_blueprint(FAQ_chat_route)
